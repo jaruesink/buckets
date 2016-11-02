9e8ba15ce60d83c492686201003f2c4fc2fddcea
Jake Ruesink <jaruesink@gmail.com>
2016-11-02 12:10:57 -0500

    opening edit drawer works now

--- src/app/components/transactions/transactionlist/transactionlist.component.ts
+++ src/app/components/transactions/transactionlist/transactionlist.component.ts
@@ -13,6 +13,19 @@ export class TransactionlistComponent {
   sort: string = 'date';
   transactions: Array<any> = [];
   constructor(public trs: TransactionsService) {}
+  edit(editing_transaction) {
+    for (let transaction of this.transactions) {
+      if (editing_transaction === transaction) {
+        transaction.editing = true;
+      } else {
+        transaction.editing = false;
+      }
+    }
+  }
+  closeEdit($event, editing_transaction) {
+    $event.stopPropagation();
+    editing_transaction.editing = false;
+  }
   ngOnChanges() {
     this.trs.unsubscribe(this.key);
     let current_month = moment(this.month).format('YYYY-MM');
@@ -21,6 +34,7 @@ export class TransactionlistComponent {
     this.trs.subscribe(this.key, current_month, next_month, (data) => {
       let transactions = [];
       for (let transaction in data) {
+        data[transaction].editing = false;
         data[transaction].$key = transaction;
         transactions.push(data[transaction]);
       }

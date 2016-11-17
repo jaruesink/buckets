760d2c0a9849fe26820b767e808c2cb3021dc068
Jake Ruesink <jaruesink@gmail.com>
2016-11-17 12:51:34 -0600

    decimals are optional now

--- src/app/pipes/money.pipe.ts
+++ src/app/pipes/money.pipe.ts
@@ -3,11 +3,21 @@ import {Pipe, PipeTransform} from "@angular/core";
 @Pipe({name: "money", pure: false})
 export class MoneyPipe implements PipeTransform {
   public transform(input) {
-    let money = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    });
+    let money;
+    if (input % 1 != 0) {
+      money = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      });
+    } else {
+      money = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+      });
+    }
+
     return money.format(input);
   }
 }
